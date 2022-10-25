7fb739e775fe87b3e5afe1585d560e8351f74fa4
HeoEunJae <dmswo159@gmail.com>
2022-10-26 01:11:52 +0900

    삭제까지 완룟

--- src/main/java/com/example/todolist/Service/TodoService.java
+++ src/main/java/com/example/todolist/Service/TodoService.java
@@ -2,10 +2,14 @@ package com.example.todolist.Service;
 
 import com.example.todolist.Domain.Todo;
 import com.example.todolist.dao.TodoRepository;
+import com.example.todolist.dto.ResponseDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TodoService {
@@ -26,5 +30,21 @@ public class TodoService {
 
     public List<Todo> getTodos() {
         return todoRepository.findAll();
+    }
+
+    public void deleteTodo(Integer id, HttpServletResponse response) throws IOException {
+        Optional<Todo> todo = todoRepository.findById(id);
+        if(todo.isPresent()){
+            todoRepository.deleteById(id);
+        } else {
+            ResponseDTO responseDTO = new ResponseDTO();
+            responseDTO.setCode("f-1");
+            responseDTO.setMessage("이미 삭제된 할 일입니다.");
+            response.setStatus(404);
+            response.setHeader("content-type", "application/json;charset-uft-8");
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.writeValue(response.getOutputStream(), responseDTO);
+
+        }
     }
 }
--- src/main/java/com/example/todolist/controller/TodoController.java
+++ src/main/java/com/example/todolist/controller/TodoController.java
@@ -2,13 +2,14 @@ package com.example.todolist.controller;
 
 import com.example.todolist.Domain.Todo;
 import com.example.todolist.Service.TodoService;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.List;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
-@Controller
+@RestController("/todos")
 public class TodoController {
 
     private final TodoService todoService;
@@ -16,16 +17,20 @@ public class TodoController {
     TodoController(TodoService todoService){
         this.todoService = todoService;
     }
-    @GetMapping("/todos")
-    @ResponseBody
+    @GetMapping("")
     public List<Todo> getTodos(){
         return todoService.getTodos();
     }
 
-    @PostMapping("/todos")
-    @ResponseBody
+    @PostMapping("")
     public List<Todo> createTodos(@RequestBody Todo todo){
         todoService.createTodo(todo);
+        return todoService.getTodos();
+    }
+
+    @DeleteMapping("/todos/{id}")
+    public List<Todo> deleteTodos(@PathVariable("id") Integer id, HttpServletResponse response) throws IOException {
+        todoService.deleteTodo(id, response);
         return todoService.getTodos();
     }
 }
